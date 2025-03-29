30a31e2deec930fb59e4d8d56184a09e546d5644
silvak <silvak.jeg@gmail.com>
2025-03-29 16:25:01 -0400

    init peoject

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-red-400">Init project</h1>
+    </>
+  );
+}
+
+export default App;
